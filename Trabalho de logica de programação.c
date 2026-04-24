7811eb2a17aa9276a487628bf465c31427edf202
Raffael <raffaelluis1226@gmail.com>
2026-04-24 14:38:14 -0300

    Update Trabalho de logica de programação.c

--- Trabalho de logica de programação.c
+++ Trabalho de logica de programação.c
@@ -1,128 +1,106 @@
 #include <stdio.h>
-#include <locale.h>
 #include <math.h>
-
+#include <locale.h>
 int main(){
-    setlocale(LC_ALL, "Portuguese");
-
-    int opcao = 0;
+setlocale (LC_ALL, "Portuguese");
+    int opcao;
 
     do {
-        printf("\n---------- MENU PRINCIPAL ----------\n\n");
-        printf("1- Resolver equacao do 1 grau.\n");
-        printf("2- Resolver equacao do 2 grau.\n");
-        printf("3- Sobre o programa.\n");
-        printf("4- Sair do programa.\n\n");
-        printf("Digite a opcao desejada: ");
+        printf("\n--------- MENU ---------\n\n");
+        printf("1 - Resolver equação 1° grau\n");
+        printf("2 - Resolver equação 2° grau\n");
+        printf("3 - Sobre o programa\n");
+        printf("4 - Sair\n");
+        printf("Escolha: ");
         scanf("%d", &opcao);
 
-        if(opcao == 1){
+        if(opcao == 1){//resolvendo equação do primeiro grau
             int a, b;
             float x;
 
-            printf("\n---------- EQUACAO DO 1 GRAU ----------\n\n");
-            printf("Formula: ax + b = 0\n\n");
-            printf("Digite o valor de A: ");
+            printf("\nEquação: ax + b = 0\n");
+
+            printf("Digite A: ");
             scanf("%d", &a);
-            printf("Digite o valor de B: ");
+            printf("Digite B: ");
             scanf("%d", &b);
 
-            // Casos especiais quando a == 0
-            if(a == 0 && b == 0){
-                printf("\nQualquer valor de x e solucao (infinitas solucoes).\n");
-            }
-            else if(a == 0 && b != 0){
-                printf("\nNao existe solucao para esses valores.\n");
-            }
-            // Caso normal
-            else if(b == 0){
-                printf("\n%dx = 0\n", a);
-                printf("x = 0\n");
-            }
-            else if(b > 0){
-                printf("\n%dx + %d = 0\n", a, b);
+            if(a == 0){
+                printf("Não da pra resolver (A = 0)\n");
+            } else {
+
+                printf("\nPasso 1:\n");
+                printf("%dx + %d = 0\n", a, b);
+
+                printf("\nPasso 2 (passando o B para o outro lado):\n");
                 printf("%dx = %d\n", a, -b);
+
+                printf("\nPasso 3 (dividindo pelo A):\n");
                 printf("x = %d / %d\n", -b, a);
-                x = (float)(-b) / a;
-                printf("x = %.2f\n", x);
-            }
-            else{ // b < 0
-                printf("\n%dx + (%d) = 0\n", a, b);
-                printf("%dx = %d\n", a, -b);
-                printf("x = %d / %d\n", -b, a);
-                x = (float)(-b) / a;
+
+                x = -b / (float)a;
+
+                printf("\nResultado:\n");
                 printf("x = %.2f\n", x);
             }
         }
 
-        else if(opcao == 2){
-            int a, b, c, delta;
-            float x1, x2;
+        else if(opcao == 2){//resolvendo equação do segundo grau
+            int a, b, c;
+            float delta, x1, x2;
 
-            printf("\n---------- EQUACAO DO 2 GRAU ----------\n\n");
-            printf("Formula: ax^2 + bx + c = 0\n\n");
-            printf("Digite o valor de A: ");
+            printf("\nEquação: ax^2 + bx + c = 0\n");
+
+            printf("Digite A: ");
             scanf("%d", &a);
-            printf("Digite o valor de B: ");
+            printf("Digite B: ");
             scanf("%d", &b);
-            printf("Digite o valor de C: ");
+            printf("Digite C: ");
             scanf("%d", &c);
 
             if(a == 0){
-                printf("\nErro: A nao pode ser zero!\n");
-            }
-            else{
-                // Exibicao limpa da equacao (ignora termos zerados)
-                printf("\n%dx^2", a);
+                printf("Não é equação do 2° grau\n");
+            } else {
 
-                if(b > 0)       printf(" + %dx", b);
-                else if(b < 0)  printf(" - %dx", -b);
+                printf("\nEquação montada:\n");
+                printf("%dx^2 + %dx + %d = 0\n", a, b, c);
 
-                if(c > 0)       printf(" + %d", c);
-                else if(c < 0)  printf(" - %d", -c);
+                printf("\nPasso 1: calcular delta\n");
+                printf("delta = b^2 - 4ac\n");
+                printf("delta = (%d)^2 - 4*%d*%d\n", b, a, c);
+                printf("delta = %d - %d\n", b*b, 4*a*c);
 
-                printf(" = 0\n\n");
-
-                // Calculo do delta com passos
-                printf("Delta = b^2 - 4ac\n");
-                printf("Delta = (%d)^2 - 4.(%d).(%d)\n", b, a, c);
-                printf("Delta = %d - (%d)\n", b*b, 4*a*c);
                 delta = b*b - 4*a*c;
-                printf("Delta = %d\n\n", delta);
 
-                if(delta > 0){
-                    x1 = (-b + sqrt(delta)) / (2.0 * a);
-                    x2 = (-b - sqrt(delta)) / (2.0 * a);
-                    printf("Duas raizes reais distintas:\n");
-                    printf("X1 = %.2f\n", x1);
-                    printf("X2 = %.2f\n", x2);
-                }
-                else if(delta == 0){
-                    x1 = (float)(-b) / (2 * a);
-                    printf("Uma raiz real (raiz dupla):\n");
-                    printf("X = %.2f\n", x1);
-                }
-                else{
-                    printf("Nao existem raizes reais (delta negativo).\n");
+                printf("delta = %.2f\n", delta);
+
+                if(delta < 0){
+                    printf("\nNão tem raiz real\n");
+                } else {
+
+                    printf("\nPasso 2: formula de Bhaskara\n");
+                    printf("x = (-b +- sqrt(delta)) / (2a)\n");
+
+                    printf("\nSubstituindo valores:\n");
+                    printf("x = (%d +- sqrt(%.2f)) / (2*%d)\n", -b, delta, a);
+
+                    x1 = (-b + sqrt(delta)) / (2*a);
+                    x2 = (-b - sqrt(delta)) / (2*a);
+
+                    printf("\nResultados:\n");
+                    printf("x1 = %.2f\n", x1);
+                    printf("x2 = %.2f\n", x2);
                 }
             }
         }
 
         else if(opcao == 3){
-            printf("\n---------- SOBRE O PROGRAMA ----------\n\n");
-            printf("Programa para resolver equacoes do 1 e 2 grau.\n");
-            printf("Desenvolvido em linguagem C.\n");
-        }
-
-        else if(opcao == 4){
-            printf("\nSaindo... Ate logo!\n");
-        }
-
-        else{
-            printf("\nOpcao invalida! Digite um numero de 1 a 4.\n");
+            printf("\nPrograma feito em linguagem C para resolver equações mostrando o passo a passo.\n\nDesenvolvido por: Raffael Luis e Cristian Rafael\n");
         }
 
     } while(opcao != 4);
+
+    printf("Fim do programa\n");
 
     return 0;
 }
